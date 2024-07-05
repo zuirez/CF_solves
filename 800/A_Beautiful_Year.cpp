dcf5788fd852ea2ef01a3ae9a0026ae3a0f5869e
ZUIREZ <rashedxshawon@gmail.com>
2024-07-05 20:46:06 +0600

    new file:   800/A_Beautiful_Year.cpp

---
+++ 800/A_Beautiful_Year.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0);//cin.tie(0);cout.tie(0);
+#define endl '\n'
+
+int arr[10000];
+
+int main()
+{
+    optimize();
+
+    int n;
+    cin>>n;
+
+    for(int i=0; i<n; )
+    {
+        cout<<'a';
+    }
+}
