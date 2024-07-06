2921ff741cdf75b7e56db0c93500cc42c56bb370
ZUIREZ <rashedxshawon@gmail.com>
2024-07-06 20:09:49 +0600

    new file:   800/A_In_Search_of_an_Easy_Problem.cpp

---
+++ 800/A_In_Search_of_an_Easy_Problem.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0);//cin.tie(0);cout.tie(0);
+#define endl '\n'
+
+int main()
+{
+    optimize();
+
+    int n,count=0;
+    cin>>n;
+
+    for(int i=0; i<n; i++)
+    {
+        int a;
+        cin>>a;
+
+        if(a==1)
+        {
+            count++;
+        }
+    }
+
+    if(count==0)
+    {
+        cout<<"EASY";
+    }
+    else
+    {
+        cout<<"HARD";
+    }
+}
