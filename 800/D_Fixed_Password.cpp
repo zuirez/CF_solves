c176b6539570d7e521f088e0d4aa40e0e6550db1
ZUIREZ <rashedxshawon@gmail.com>
2025-04-04 19:52:45 +0600

    Add solution for Fixed Password problem

---
+++ 800/D_Fixed_Password.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define optimize() ios_base::sync_with_stdio(0);//cin.tie(0);cout.tie(0);
+#define endl '\n'
+ 
+int main()
+{
+    optimize();
+ 
+    while(true)
+    {
+        int a;
+        cin>>a;
+ 
+        if(a == 1999)
+        {
+            cout<<"Correct"; 
+            break;
+        }
+        else
+        {
+            cout<<"Wrong"<<endl;
+        }
+    }
+}
